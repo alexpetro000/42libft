2415a277e4ba8f4adb376299e5a40ee9d68d8306
Alex <alexpetro000@gmail.com>
2020-05-06 01:05:37 +0300

    small optimizations

--- src/list/ft_lstmap.c
+++ src/list/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: afreeze <afreeze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 15:17:43 by afreeze           #+#    #+#             */
-/*   Updated: 2020/05/05 17:36:36 by afreeze          ###   ########.fr       */
+/*   Updated: 2020/05/06 00:14:49 by afreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,23 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
+	t_list	*res_cp;
 	t_list	*tmp;
 
-	if (!f || !del)
+	if (!f || !del || !lst || !(tmp = f(lst->content)))
 		return (NULL);
-	res = NULL;
-	while (lst)
+	res = ft_lstnew(tmp);
+	res_cp = res;
+	while (lst->next)
 	{
-		tmp = f(lst->content);
-		if (!tmp)
+		lst = lst->next;
+		if (!(tmp = f(lst->content)))
 		{
 			ft_lstclear(&res, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&res, ft_lstnew(tmp));
-		lst = lst->next;
+		res->next = ft_lstnew(tmp);
+		res = res->next;
 	}
-	return (res);
+	return (res_cp);
 }
