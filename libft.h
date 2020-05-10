643ea93f9fef6eeaacc916c3c247f0055618e68c
Alex <alexpetro000@gmail.com>
2020-05-10 19:08:43 +0300

    removed all _bonus ((((

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: alexp <alexp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 16:37:48 by alexp             #+#    #+#             */
-/*   Updated: 2020/05/07 01:43:45 by afreeze          ###   ########.fr       */
+/*   Updated: 2020/05/10 18:56:44 by afreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,10 @@ void			*ft_calloc(size_t nmemb, size_t size);
 */
 size_t			ft_strlen(const char *s);
 char			*ft_strcpy(char *dest, const char *src);
-char			*ft_strncpy(char *dest, const char *src, size_t n);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
-char			*ft_strstr(const char *big, const char *little);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 
@@ -50,37 +48,18 @@ char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
 char			**ft_split(char const *s, char c);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void			ft_putchar(char c);
 void			ft_putchar_fd(char c, int fd);
-void			ft_putstr(char *s);
 void			ft_putstr_fd(char *s, int fd);
-void			ft_putendl(char *s);
 void			ft_putendl_fd(char *s, int fd);
-void			ft_putnbr(int n);
 void			ft_putnbr_fd(int n, int fd);
-
-int				ft_strcntchr(const char *s, char c);
-char			*ft_strupr(char *lower);
-char			*ft_strlwr(char *lower);
 
 /*
 ** Char functions
 */
-int				ft_isalnum(int c);
 int				ft_isalpha(int c);
-int				ft_iscntrl(int c);
 int				ft_isdigit(int c);
-int				ft_isgraph(int c);
-int				ft_islower(int c);
 int				ft_isprint(int c);
-int				ft_ispunct(int c);
-int				ft_isspace(int c);
-int				ft_isupper(int c);
-int				ft_isxdigit(int c);
-
 int				ft_isascii(int c);
-int				ft_isblank(int c);
-
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 
@@ -89,12 +68,6 @@ int				ft_toupper(int c);
 */
 int				ft_atoi(const char *s);
 char			*ft_itoa(int n);
-
-/*
-** Math
-*/
-int				ft_min(int a, int b);
-int				ft_max(int a, int b);
 
 /*
 ** List
